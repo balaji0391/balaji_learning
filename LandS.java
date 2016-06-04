7c7c35aeecaf608624e2548286f4b204426d565d
Rohit <nirohit@gmail.com>
2016-06-04 15:43:54 +0530

    Suggested changes to LandS.java

--- LandS.java
+++ LandS.java
@@ -2,10 +2,12 @@ public class LandS
 {
 	public static void main(String args[])
 	{
+		//Arrays need to be zero indexed
 		int a[]=new int[]{23,33,43,53};
 		int small=a[1];
 		int large=a[1];
 
+		//Use array.length instead of hardcoding values
 		for(int i=1;i<4;i++)
 		{
 			if(a[i]>large)
